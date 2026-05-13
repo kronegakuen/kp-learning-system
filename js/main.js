8e8c5e1c11a6ca223d8e967b5365f859b9010eda
kronegakuen <249214041+kronegakuen@users.noreply.github.com>
2026-05-14 00:30:14 +0900

    生成ボタン

--- js/main.js
+++ js/main.js
@@ -1,17 +1,3 @@
-console.log("JS読み込みOK");
-
-let DATA = [];
-
-async function loadData() {
-  const res = await fetch("https://script.google.com/macros/s/AKfycbyTF8pd6_KVnw-qS4tIJLFbIKaoARErmH2hlpLax3LIdoc2MSoAkbJMgz052aV-SAZzxw/exec");
-  DATA = await res.json();
-}
-
-function generate() {
-  console.log("ボタン押された");
-
-  document.getElementById("output").innerHTML =
-    "ボタンは動いてる";
-}
-
-loadData();
+window.generate = function () {
+  alert("ボタン動いた！");
+};
