c52149469ce7a87c3d821407b872f8884b74adb9
kronegakuen <249214041+kronegakuen@users.noreply.github.com>
2026-05-14 00:00:44 +0900

    connect sheets

--- js/main.js
+++ js/main.js
@@ -1,4 +1,28 @@
-function generate() {
-  document.getElementById("output").innerHTML =
-    "ボタン動いてる";
+let DATA = [];
+
+async function loadData() {
+  const res = await fetch("https://script.google.com/macros/s/AKfycbyTF8pd6_KVnw-qS4tIJLFbIKaoARErmH2hlpLax3LIdoc2MSoAkbJMgz052aV-SAZzxw/exec");
+  DATA = await res.json();
 }
+
+function generate() {
+  const grade = document.getElementById("grade").value;
+  const theme = document.getElementById("theme").value;
+  const num = Number(document.getElementById("num").value);
+
+  let filtered = DATA.filter(d =>
+    d.grade.includes(grade) &&
+    (theme === "" || d.theme.includes(theme))
+  );
+
+  filtered = filtered.sort(() => Math.random() - 0.5).slice(0, num);
+
+  document.getElementById("output").innerHTML =
+    filtered.map((q, i) => `
+      <div>
+        <b>${i + 1}.</b> ${q.phrase}（${q.meaning}）
+      </div>
+    `).join("");
+}
+
+loadData();
